b7891e35ef3ff2601414d9baac494400095cdce0
yossy <hirata@hogehoge.com>
2023-11-30 11:57:31 +0900

    add class talker.py

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -2,17 +2,20 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
+class Talker():
+    def __init__(self): #init イニシャライズ 
+        self.pub = node.create_publisher(Int16,"countup",10)
+        self.n = 0
+
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Int16,"countup",10)
-n = 0
+talker = Talker() #initが実行、（）の引数はなし。classでは引数にselfがあるが書かなくてよい
 
 def cb ():
-    global n
     msg = Int16()
-    msg.data = n
-    pub.publish(msg)
-    n += 1
+    msg.data = talker.n
+    talker.pub.publish(msg)
+    talker.n += 1
 
 node.create_timer(0.5,cb)
 rclpy.spin(node)
